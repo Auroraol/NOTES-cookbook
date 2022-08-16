a9a6d8cc32e6b14372d9e5f409765e1a36e056dc
jingdao <1665834268@qq.com>
2022-08-16 11:02:58 +0800

    2022/08/16

--- backups.sh
+++ backups.sh
@@ -7,8 +7,12 @@ DATETIME=$(date "+%Y/%m/%d")
 # 执行git命令
 git add .
 git commit -m "$DATETIME"
+echo "$DATETIME"
 git remote set-url origin https://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@github.com/Auroraol/notes-cookbook.git
 git push
 
 # 删除缓存
 rm -rvf /usr/local/nginx/html/blog/runtime/cache
+
+
+echo "备份成功"
