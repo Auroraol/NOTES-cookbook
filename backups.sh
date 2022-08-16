9ca0dca6204057683f69b9b037e1327ea4af497a
jingdao <1665834268@qq.com>
2022-08-16 20:09:08 +0800

    2022/08/16

--- backups.sh
+++ backups.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 cd /mnt/c/Users/16658/Desktop/X-NOTES
+CURDIR=$(dirname)
+if [ CURDIR = "/mnt/c/Users/16658/Desktop/X-NOTES" ]
+then
+    echo "####### 进入自己的项目 #######"
+fi
+
+make html
 
 DATETIME=$(date "+%Y/%m/%d")
 
 # 执行git命令
 git add .
 git commit -m "$DATETIME"
-echo "$DATETIME"
-git remote set-url origin https://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/Auroraol/notes-cookbook.git
+echo "####### commit #######"
+git remote set-url origin https://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/Auroraol/notes-cookbook.git
+echo "####### 开始推送 #######"
 git push
 
 # 删除缓存
 rm -rvf /usr/local/nginx/html/blog/runtime/cache
 
-
-echo "备份成功"
+echo "####### 推送成功 #######"
